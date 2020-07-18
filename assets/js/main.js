3628a2b814b83a8232ca8608e3e23ff2cdb16d27
Alexandr Somov <aleksandr.somov2011@gmail.com>
2020-07-18 09:05:37 +0300

    style: button burger done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,11 @@
-
 $(document).ready(function () {
-
-  $('.header-navbar__hamburger').on('click', function () {
-    $(this).toggleClass('active');
-    $('.header-navbar__list').toggleClass('active');
+  $('.header-menu-link-btn').click(function () {
+    $(this).toggleClass('open');
+    $('.header-menu-link-list').stop(true, true).fadeToggle(500);
   });
-
+  $(window).resize(function () {
+    if($(window).width() >= 767.8){
+      $('.header-menu-link-list').css('display', '');
+    }
+  });
 });
